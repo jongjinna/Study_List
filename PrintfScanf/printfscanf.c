08cdd356e04ec3b7af6fd0c0c552a1f4597a2c66
jongjinna <njjwa1004@naver.com>
2021-12-18 04:00:53 +0900

    :zap: printf&scanf variable

---
+++ PrintfScanf/printfscanf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+  // 정수형 변수
+  // int age = 12;
+  // printf("%d\n", age);
+  // age = 13;
+  // printf("%d\n", age);
+
+  // 실수형 변수
+  // float f = 46.5f;
+  // printf("%.2f\n", f);
+  // double d = 4.428;
+  // printf("%.2lf\n", d); // double은 lf
+
+  return 0;
+}
