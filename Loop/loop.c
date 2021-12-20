fac91843ac5c455d688f406569ee298a40107e0f
jongjinna <njjwa1004@naver.com>
2021-12-20 15:25:38 +0900

    :zap: study loop 21.12.20.

--- Loop/loop.c
+++ Loop/loop.c
@@ -1,6 +1,60 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello world\n");
+  // printf("Hello world\n");
+  // printf("Hello world\n");
+  // printf("Hello world\n");
+  // printf("Hello world\n");
+  
+  // int a = 10;
+  // printf("a 는 %d\n", a);
+  // a++;
+  // printf("a 는 %d\n", a);
+  
+  // int b = 20;
+  // printf("b 는 %d\n", ++b); // ++한 뒤 b
+  // printf("b 는 %d\n", b++); // b한 뒤 ++
+  // printf("b 는 %d\n", b);
+
+  // int i = 1;
+  // printf("Hello world %d\n", i++);
+  // printf("Hello world %d\n", i++);
+  // printf("Hello world %d\n", i++);
+  // printf("Hello world %d\n", i++);
+  // printf("Hello world %d\n", i++);
+  // printf("Hello world %d\n", i++);
+  // printf("Hello world %d\n", i++);
+  // printf("Hello world %d\n", i++);
+
+  // 반복문 (for, while, do while)
+  // for (int i = 1; i <= 10; i++) {
+  //   printf("Hello world %d\n", i);
+  // }
+
+  // int i = 1;
+  // while (i <= 10) {
+  //   printf("Hello world %d\n", i++);
+  // }
+  
+  // int i = 1;
+  // do {
+  //   printf("Hello world %d\n", i++);
+  // } while (i <= 10);
+
+  // 2중 반복문
+  // for (int i = 1; i <= 3; i++) {
+  //   printf("첫번째 반복문 : %d\n", i);
+  //   for (int j = 1; j <= 5; j++) {
+  //     printf("  두번째 반복문 : %d\n", j);
+  //   }
+  // }
+
+  // 구구단
+  // for (int i = 2; i <= 9; i++) {
+  //   printf("%d단 계산\n", i);
+  //   for(int j = 1; j <= 9; j++) {
+  //     printf("  %d x %d = %d \n", i, j, i*j);
+  //   }
+  // }
   return 0;
 }
