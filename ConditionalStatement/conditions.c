8fea17b261c98682a8feccef784ac1ffabb3acf1
jongjinna <njjwa1004@naver.com>
2021-12-21 18:02:55 +0900

    :art: Study condition 21.12.21.

---
+++ ConditionalStatement/conditions.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(void) {
+  // 버스 가정, 학생 / 일반인 구분 (20세 기준)
+  // int age = 25;
+  // if (age >= 20) {
+  //   printf("일반인 입니다.\n");
+  // }
+  // else {
+  //   printf("학생입니다.\n");
+  // }
+
+  // 초등학생 (8~13) / 중학생 (14~16) / 고등학생 (17~19)
+  // int age = 25;
+  // if (age >= 8 && age <= 13) {
+  //   printf("초등학생 입니다.\n");
+  // }
+  // else if (age >= 14 && age <= 16) {
+  //   printf("중학생 입니다.\n");
+  // }
+  // else if (age >= 17 && age <= 19) {
+  //   printf("고등학생 입니다.\n");
+  // }
+  // else {
+  //   printf("학생이 아닙니다.\n");
+  // }
+
+  // break / continue
+  // for (int i = 1; i <= 30; i++) {
+  //   if (i >= 6) {
+  //     printf("나머지 학생은 집에 가세요.\n");
+  //     break;
+  //   }
+  //   printf("%d번 학생은 발표 준비를 하세요.\n", i);
+  // }
+
+  // for (int i = 1; i <= 30; i++) {
+  //   if (i >= 6 && i <= 10) {
+  //     if (i == 7) {
+  //       printf("%d번 학생은 결석입니다.\n", i);
+  //       continue;
+  //     }
+  //     printf("%d번 학생은 발표 준비 하세요.\n", i);
+  //   }
+  // }
+
+  
+  return 0;
+}
