8b2ceeb28062c901665e6fad6489523390088746
jongjinna <njjwa1004@naver.com>
2022-01-10 18:59:40 +0900

    :zap: sql connection

--- DataAnalysis/SQLstudy/test.py
+++ DataAnalysis/SQLstudy/test.py
@@ -1,2 +1,11 @@
-from sqlalchemy import create_engine
-print("hello world!")
+import sqlalchemy as db
+print(1)
+
+engine = db.create_engine('mysql+pymysql://root:////@localhost/MyData')
+
+connection = engine.connect()
+metadata = db.MetaData()
+table = db.Table('language', metadata, autoload=True, autoload_with=engine)
+
+print(table.columns.keys())
+print(1)
