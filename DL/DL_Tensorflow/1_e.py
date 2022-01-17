7d73cd0813e67e1d64b71eb33139fbb27346651b
jongjinna <njjwa1004@naver.com>
2022-01-17 22:49:19 +0900

    :art: study regression on tensorflow 1

--- DL/DL_Tensorflow/1_e.py
+++ DL/DL_Tensorflow/1_e.py
@@ -1,12 +1,23 @@
 import tensorflow as tf
 
+# Data
 x_data = [1, 2, 3, 4, 5]
-
 y_data = [1, 2, 3, 4, 5]
 
+# W, b initialize
 W = tf.Variable(2.9)
 b = tf.Variable(0.5)
 
-hypothesis = W*x_data + b
+learning_rate = 0.01
 
-print(hypothesis)
+for i in range(1000):
+  # Gradient descent
+  with tf.GradientTape() as tape:
+    hypothesis = W*x_data + b
+    cost = tf.reduce_mean(tf.square(hypothesis-y_data))
+  W_grad, b_grad = tape.gradient(cost, [W,b])
+  W.assign_sub(learning_rate * W_grad)
+  b.assign_sub(learning_rate * b_grad)
+  # assign_sub 는 python의 a -= b 와 같은 역할
+  if i % 10 == 0:
+    print("{:5}|{:10.4}|{:10.4}|{:10.6f}".format(i, W.numpy(), b.numpy(), cost))
