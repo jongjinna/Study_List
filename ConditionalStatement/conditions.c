9f48dda83bc5eefd5c2a4b6573f8daae3aed02c7
jongjinna <njjwa1004@naver.com>
2022-02-12 15:57:42 +0900

    :memo: study condition up and down

--- ConditionalStatement/conditions.c
+++ ConditionalStatement/conditions.c
@@ -1,3 +1,5 @@
+#include <time.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(void) {
@@ -45,10 +47,10 @@ int main(void) {
   // }
 
   // and(&&) & or(||)
-  int a = 10;
-  int b = 11;
-  int c = 12;
-  int d = 13;
+  // int a = 10;
+  // int b = 11;
+  // int c = 12;
+  // int d = 13;
   // if (a == b && c == d) {
   //   printf("a == b && c == d\n");
   // }
@@ -57,7 +59,72 @@ int main(void) {
   //   printf("a != b || c != d\n");
   // }
   
+  // 가위 0 / 바위 1 / 보 2
+  // srand(time(NULL));
+  // int i = rand() % 3;
+  // if (i == 0) {
+  //   printf("가위\n");
+  // }
+  // else if (i == 1) {
+  //   printf("바위\n");
+  // }
+  // else {
+  //   printf("보\n");
+  // }
 
+  // srand(time(NULL));
+  // int i = rand() % 3;
+  // switch (i)
+  // {
+  // case 0: printf("가위\n"); break;
+  // case 1: printf("바위\n"); break;
+  // case 2: printf("보\n"); break;
+  // default: printf("몰라요\n");
+  // }
+
+  // // 초등학생 (8~13) / 중학생 (14~16) / 고등학생 (17~19)
+  // int age = 15;
+  // switch(age) {
+  //   case 8:
+  //   case 9:
+  //   case 10:
+  //   case 11:
+  //   case 12:
+  //   case 13: printf("초등학생 입니다.\n"); break;
+  //   case 14:
+  //   case 15:
+  //   case 16: printf("중학생 입니다.\n"); break;
+  //   case 17:
+  //   case 18:
+  //   case 19: printf("고등학생 입니다.\n"); break;
+  //   default: printf("학생이 아닙니다.\n");
+  // }
+
+  // UP & Down
+  srand(time(NULL));
+  int num = rand() % 100 + 1;
+  printf("숫자 : %d\n", num);
+  int answer = 0;
+  int chance = 5;
+  while (1) {
+    printf("남은 기회 %d번 \n", chance--);
+    printf("숫자를 맞춰보세요 (1~100) : ");
+    scanf("%d", &answer);
+    if (answer == num) {
+      printf("정답입니다.\n");
+      break;
+    }
+    else if (answer > num) {
+      printf("숫자가 너무 큽니다.\n");
+    }
+    else {
+      printf("숫자가 너무 작습니다.\n");
+    }
+    if (chance == 0) {
+      printf("기회가 없습니다.\n");
+      break;    
+    }
+  }
 
   return 0;
 }
