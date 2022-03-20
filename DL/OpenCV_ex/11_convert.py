c588ba85eca9726608c2c78283a8d9e3679a517c
jongjinna <njjwa1004@naver.com>
2022-03-21 00:25:55 +0900

    :memo: convert_2

---
+++ DL/OpenCV_ex/11_convert.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+# img = cv2.imread('./DL/OpenCV_ex/newspaper.jpg')
+
+# width, height = 640, 240
+
+# src = np.array([[511, 352], [1008, 345], [1122, 584], [455, 594]], dtype=np.float32)
+# dst = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32)
+
+# matrix = cv2.getPerspectiveTransform(src, dst)
+# result = cv2.warpPerspective(img, matrix, (width, height))
+
+# cv2.imshow('img', img)
+# cv2.imshow('result', result)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+img = cv2.imread('./DL/OpenCV_ex/poker.jpg')
+
+width, height = 530, 710
+
+src = np.array([[702, 143], [1133, 413], [726, 1007], [276, 700]], dtype=np.float32)
+dst = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32)
+
+matrix = cv2.getPerspectiveTransform(src, dst)
+result = cv2.warpPerspective(img, matrix, (width, height))
+
+cv2.imshow('img', img)
+cv2.imshow('result', result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
