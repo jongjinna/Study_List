511ccb18c0c72d361d447db00bb74e06563fa386
jongjinna <njjwa1004@naver.com>
2022-08-12 18:23:40 +0900

    :art: java

---
+++ Duoen.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Duoen {
+  public static void main(String[] args) {
+   helloWorld(); 
+   duoenArrayList(args);
+  }
+
+  public static void helloWorld() {
+    System.out.println("Hello World!");
+  }
+  
+  public static void duoenArrayList(String[] args) { 
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add(0, "Volvo");
+    cars.add(1, "Volvo");
+    cars.add(2, "Ford");
+    cars.add(3, "Mazda");
+    System.out.println(cars.remove("Volvo"));
+    // cars.remove("Volvo");
+    // System.out.println(cars);
+  } 
+}
--- Sample.java
+++ Sample.java
@@ -2,6 +2,8 @@ public class Sample {
   public static void main(String[] args) {
     int a = 10;
     int b = 5;
+    String hello = "hello";
+    System.out.println(hello);
     System.out.println(a + b);
     System.out.println(a - b);
     System.out.println(a * b);
