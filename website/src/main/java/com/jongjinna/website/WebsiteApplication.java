d65e4b4bb180e7f37a7152318df488bed4c01939
jongjinna <njjwa1004@naver.com>
2022-09-18 11:58:55 +0900

    :sparkles: mysql

--- website/src/main/java/com/jongjinna/website/WebsiteApplication.java
+++ website/src/main/java/com/jongjinna/website/WebsiteApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebsiteApplication {
 
 	public static void main(String[] args) {
+    try {
 		SpringApplication.run(WebsiteApplication.class, args);
-	}
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
 }
